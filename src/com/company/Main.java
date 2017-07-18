d80fbf3ee9c138b660c7fda780295a302f19a824
Dominik <domiloo@hotmail.com>
2017-07-18 18:50:46 +0200

    länger

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("adsfasdf");
+        System.out.println("adsfaresdgxfdgjlksdf");
         Book book1 = new Book();
         System.out.println(book1.getNumber());
     }
